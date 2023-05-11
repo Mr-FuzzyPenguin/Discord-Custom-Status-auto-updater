0e838342688a4bc73c3d92cc16504b4d3123d44a
Mr-FuzzyPenguin <scokro7141@gmail.com>
2023-05-11 02:50:17 -0400

    Fixed bugs in discord_message_gen.py

--- discord_message_gen.py
+++ discord_message_gen.py
@@ -8,9 +8,6 @@ import emoji
 import time
 from datetime import datetime, timedelta
 
-# not supported (yet)
-# timestamp = "21" + unix
-
 # Check python version!
 import sys
 
@@ -18,8 +15,10 @@ if sys.version_info[0:2] != (3, 11):
     raise Exception("This module requires python 3.11!")
 
 
-def message_generate(message, emoji_symbol, presence_choice, expiry=False, custom_expiry=False):
-    ## Useful sub-functions:
+def message_generate(
+    message, emoji_symbol, presence_choice, expiry=False, custom_expiry=False
+):
+    ## Useful hex-functions:
 
     # Converts a string to its hexadecimal representation, return str.
     convert_string_to_hex = lambda text: text.encode("utf-8").hex(" ")
@@ -32,14 +31,17 @@ def message_generate(message, emoji_symbol, presence_choice, expiry=False, custo
         2:
     ].rjust(2, "0")
 
-    ## Constants:
-
+    ## Helpful constants:
     # The presence table
     presence = {
         "online": "06 6f 6e 6c 69 6e 65 12",
         "idle": "04 69 64 6c 65 12",
         "dnd": "03 64 6e 64 12",
     }
+
+    # For time
+    now = datetime.now()
+    HOUR = 3600
 
     # emojis, constant offset, symbol handler, and utf8 -> hex formatter
     emoji_offset = 6 if emoji_symbol else 0
@@ -53,40 +55,51 @@ def message_generate(message, emoji_symbol, presence_choice, expiry=False, custo
     # Convert message to hex.
     hexified_text = convert_string_to_hex(message)
 
-    # Generate timestamp
-    now = datetime.now()
-    HOUR = 3600
-
+    ## Generating expiry timestamp.
+    # Useful time-functions:
     # Time from now, returns datetime
-    time_from_now = lambda sec: now+timedelta(seconds=sec)
+    time_from_now = lambda sec: now + timedelta(seconds=sec)
     # Converts to unix with millisecond precision
-    unix_time_converter = lambda t: int(time.mktime(t.timetuple())*1e3 + t.microsecond/1e3)
+    unix_time_converter = lambda t: int(
+        time.mktime(t.timetuple()) * 1e3 + t.microsecond / 1e3
+    )
     # converts unix to hex with "prettifying".
-    hex_time_converter = lambda t: spacer('0'+hex(t)[2:]).split()[::-1]
+    hex_time_converter = lambda t: spacer("0" + hex(t)[2:]).split()[::-1]
 
-    # Puts all the functions together!
-    all_time_control = lambda t: ' '.join(hex_time_converter(unix_time_converter(time_from_now(t))))
-    specific_time_control = lambda y, mo, d, h, m, s, ms: ' '.join(hex_time_converter(unix_time_converter( datetime(y, mo, d, h, m, s, ms))))
+    # Function merger:
+    all_time_control = lambda t: " ".join(
+        hex_time_converter(unix_time_converter(time_from_now(t)))
+    )
+    specific_time_control = lambda y, mo, d, h, m, s, ms: " ".join(
+        hex_time_converter(unix_time_converter(datetime(y, mo, d, h, m, s, ms)))
+    )
 
+    # pre-made settings
     hour_from_now = all_time_control(HOUR)
-    four_hours_from_now = all_time_control(4*HOUR)
-    half_hour_from_now = all_time_control(0.5*HOUR)
-    # midnight of next day
+    four_hours_from_now = all_time_control(4 * HOUR)
+    half_hour_from_now = all_time_control(0.5 * HOUR)
 
-    midnight = (specific_time_control(now.year, now.month, now.day+1, 0, 0, 0, 0))
-    expiry_table={
-        "hour" : hour_from_now,
-        "four hours" : four_hours_from_now,
-        "half hour" : half_hour_from_now,
-        "midnight" : midnight,
-        "custom" : None if not custom_expiry else specific_time_control(*custom_expiry) if len(custom_expiry) != 1 else all_time_control(*custom_expiry)
+    # midnight of next day
+    midnight = specific_time_control(now.year, now.month, now.day + 1, 0, 0, 0, 0)
+
+    # Expiry table for choosing expiry time.
+    expiry_table = {
+        "hour": hour_from_now,
+        "four hours": four_hours_from_now,
+        "half hour": half_hour_from_now,
+        "midnight": midnight,
+        "custom": None
+        if not custom_expiry
+        else specific_time_control(*custom_expiry)
+        if len(custom_expiry) != 1
+        else all_time_control(*custom_expiry),
     }
 
-    # print(hour_from_now)
-
     # remaining byte numeric representations.
-    n1 = pad(hexified_text, len(presence_choice) + 8 + emoji_offset + int(bool(expiry))*9)
-    n2 = pad(hexified_text, 2 + emoji_offset + int(bool(expiry))*9)
+    n1 = pad(
+        hexified_text, len(presence_choice) + 8 + emoji_offset + int(bool(expiry)) * 9
+    )
+    n2 = pad(hexified_text, 2 + emoji_offset + int(bool(expiry)) * 9)
     n3 = pad(hexified_text)
 
     return b64encode(
@@ -109,4 +122,3 @@ def message_generate(message, emoji_symbol, presence_choice, expiry=False, custo
 
 # DEBUG
 # print(message_generate("Hello World!", ":penguin:", "dnd", "custom", [5]))
-# WhEKBQoDZG5kEggKABoE8J+Qpw==

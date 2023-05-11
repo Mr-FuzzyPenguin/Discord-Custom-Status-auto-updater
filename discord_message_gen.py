501bc0d5555181bfe2fc03c1c5d7e6ec335ddb6d
Mr-FuzzyPenguin <scokro7141@gmail.com>
2023-05-11 01:45:31 -0400

    Added support for time in the discord-message-gen module.

--- discord_message_gen.py
+++ discord_message_gen.py
@@ -4,17 +4,29 @@ from base64 import b64encode
 # For emoji support!
 import emoji
 
+# Timestamp generator
+import time
+from datetime import datetime, timedelta
+
 # not supported (yet)
 # timestamp = "21" + unix
 
+# Check python version!
+import sys
 
-def message_generate(message, emoji_symbol, presence_choice):
+if sys.version_info[0:2] != (3, 11):
+    raise Exception("This module requires python 3.11!")
+
+
+def message_generate(message, emoji_symbol, presence_choice, expiry=False, custom_expiry=False):
     ## Useful sub-functions:
 
-    # Converts a string to its hexadecimal representation
-    convert_string_to_hex = lambda text: " ".join(
-        [str(hex(ord(i)))[2:].rjust(2, "0") for i in text]
-    )
+    # Converts a string to its hexadecimal representation, return str.
+    convert_string_to_hex = lambda text: text.encode("utf-8").hex(" ")
+
+    # adds a space every 2 chars.
+    spacer = lambda s: " ".join(s[i : i + 2] for i in range(0, len(s), 2))
+
     # Takes a hex sequence (str). Finds number of bytes, and pads it (into hex).
     pad = lambda hex_sequence, offset=0: str(hex(len(hex_sequence.split()) + offset))[
         2:
@@ -36,16 +48,45 @@ def message_generate(message, emoji_symbol, presence_choice):
         if len(emoji_symbol) == 1
         else (emoji.emojize(emoji_symbol, language="alias"))
     )
-    hex_emoji = " ".join(
-        repr(emoji_symbol.encode("utf-8"))[:-1].split("\\x")[1:] if emoji_symbol else ""
-    )
+    hex_emoji = convert_string_to_hex(emoji_symbol) if emoji_symbol else ""
 
     # Convert message to hex.
     hexified_text = convert_string_to_hex(message)
 
-    # numeric representations.
-    n1 = pad(hexified_text, len(presence_choice) + 8 + emoji_offset)
-    n2 = pad(hexified_text, 2 + emoji_offset)
+    # Generate timestamp
+    now = datetime.now()
+    HOUR = 3600
+
+    # Time from now, returns datetime
+    time_from_now = lambda sec: now+timedelta(seconds=sec)
+    # Converts to unix with millisecond precision
+    unix_time_converter = lambda t: int(time.mktime(t.timetuple())*1e3 + t.microsecond/1e3)
+    # converts unix to hex with "prettifying".
+    hex_time_converter = lambda t: spacer('0'+hex(t)[2:]).split()[::-1]
+
+    # Puts all the functions together!
+    all_time_control = lambda t: ' '.join(hex_time_converter(unix_time_converter(time_from_now(t))))
+    specific_time_control = lambda y, mo, d, h, m, s, ms: ' '.join(hex_time_converter(unix_time_converter( datetime(y, mo, d, h, m, s, ms))))
+
+    hour_from_now = all_time_control(HOUR)
+    four_hours_from_now = all_time_control(4*HOUR)
+    half_hour_from_now = all_time_control(0.5*HOUR)
+    # midnight of next day
+    midnight = (specific_time_control(now.year, now.month, now.day+1, 0, 0, 0, 0))
+
+    expiry_table={
+        "hour" : hour_from_now,
+        "four hours" : four_hours_from_now,
+        "half hour" : half_hour_from_now,
+        "midnight" : midnight,
+        "custom" : None if not custom_expiry else specific_time_control(*custom_expiry)
+    }
+
+    # print(hour_from_now)
+
+    # remaining byte numeric representations.
+    n1 = pad(hexified_text, len(presence_choice) + 8 + emoji_offset + int(bool(expiry))*9)
+    n2 = pad(hexified_text, 2 + emoji_offset + int(bool(expiry))*9)
     n3 = pad(hexified_text)
 
     return b64encode(
@@ -58,7 +99,8 @@ def message_generate(message, emoji_symbol, presence_choice):
                     n2,  # (remaining bits)
                     "0a %s" % n3,  # custom status length
                     hexified_text,  # custom status in hex.
-                    "%s" % ("1a 04 " + hex_emoji if emoji_symbol else ""),  # "21",
+                    "%s" % ("1a 04 " + hex_emoji if emoji_symbol else ""),
+                    "%s" % ("21" + expiry_table[expiry] + "00 00" if expiry else ""),
                 ]
             )
         )
@@ -66,4 +108,5 @@ def message_generate(message, emoji_symbol, presence_choice):
 
 
 # DEBUG
-# print(message_generate("hello world!", ":penguin:", "dnd"))
+print(message_generate("Hello World!", ":penguin:", "dnd", "custom", [2023, 5, 11, 1, 42, 48, 0]))
+# WhEKBQoDZG5kEggKABoE8J+Qpw==
